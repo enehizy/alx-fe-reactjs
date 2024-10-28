54efa8cb8b19a790967cf9748056c0da3c47b2d2
enehizy <osamudiameenehizena@gmail.com>
2024-10-28 09:53:08 +0100

    add header,maincontent,footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,12 +8,12 @@ import Footer from './Footer';
 function App() {
 
   return (
-  <>
-  <Header/>
-  <MainContent/>
-  <Footer/>
-  <WelcomeMessage/>
-  </>
+  <div>
+      <Header/>
+      <MainContent/>
+      <Footer/>
+      <WelcomeMessage/>
+  </div>
       
   )
 }
