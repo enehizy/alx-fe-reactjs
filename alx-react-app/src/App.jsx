1dffd62664ad00176da88d7595662bcb700c3f6f
enehizy <osamudiameenehizena@gmail.com>
2024-10-28 10:03:56 +0100

    duplicated files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,17 +4,18 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-  <>
-      <Header/>
-      <MainContent/>
-      <Footer/>
-      <WelcomeMessage/>
-  </>
-      
+    <>
+    <Header/>
+    <MainContent/>
+    <Footer/>
+     <WelcomeMessage/>
+    </>
+   
+     
   )
 }
 
