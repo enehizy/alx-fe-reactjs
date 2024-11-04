edbade734222557d622572b56517d34e4b07374b
enehizy <osamudiameenehizena@gmail.com>
2024-11-04 14:41:36 +0100

    moved files to props file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,17 +2,14 @@
 import './App.css'
 import Counter from './components/Counter'
 import WelcomeMessage from './components/WelcomeMessage'
-import UserContext from './UserContext';
-function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
+function App() {
+ 
   return (
     <>
   
     <Counter/>
-    <UserContext.Provider value={userData}>
-       <ProfilePage />
-    </UserContext.Provider>
+   
     
     </>
    
