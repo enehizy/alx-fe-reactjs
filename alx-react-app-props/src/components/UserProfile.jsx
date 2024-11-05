e99bb7436adfca16db15c98e8b6f0d27f35b280d
enehizy <osamudiameenehizena@gmail.com>
2024-11-05 11:43:08 +0100

    added context to userProile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,12 @@
-const UserProfile = (props) => {
+import React,{useContext} from "react";
+import userContext from './UserContext';
+const UserProfile = () => {
+  let userData =useContext(userContext);
     return (
       <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
+        <h2>{userData.name}</h2>
+        <p>Age: {userData.age}</p>
+        <p>Bio: {userData.bio}</p>
       </div>
     );
   };
