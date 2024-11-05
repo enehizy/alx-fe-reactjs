5070fc239f438269d2af8ae00bb345d3304606b0
enehizy <osamudiameenehizena@gmail.com>
2024-11-05 11:38:30 +0100

    moved userContext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,7 +5,7 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import UserProfile from './components/UserProfile'
-import UserContext from './UserContext';
+import UserContext from './components/UserContext';
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
