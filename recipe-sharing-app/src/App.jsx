d250dd87db68c526bf82ab2c777c6475523dba01
enehizy <osamudiameenehizena@gmail.com>
2024-11-12 13:35:22 +0100

    added router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -7,18 +7,35 @@ import RecipeList from './components/RecipeList'
 import EditRecipeForm from './components/EditRecipeForm'
 import FavoritesList from './components/FavoritesList'
 import RecommendationsList from './components/RecommendationsList'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import RecipeDetails from './components/RecipeDetails'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RecipeDetails/>
+  },
+ 
+]);
 function App() {
  
 
   return (
-    <>
-    <RecipeList/>
-     <AddRecipeForm/>
-     <EditRecipeForm/>
-     <FavoritesList/>
-     <RecommendationsList/>
-    </>
+    <RouterProvider router={router} >
+          <RecipeList/>
+        <AddRecipeForm/>
+        <EditRecipeForm/>
+        <FavoritesList/>
+        <RecommendationsList/>
+      
+      </RouterProvider>
+   
+   
   )
 }
 
