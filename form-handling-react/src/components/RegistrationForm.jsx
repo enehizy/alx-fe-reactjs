b327a598a091c2847f3ff48d1f0a851365c23018
enehizy <osamudiameenehizena@gmail.com>
2024-11-19 19:14:21 +0100

    added on more input filed

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -2,19 +2,21 @@ import React, { useState } from 'react'
 
 function RegistrationForm() {
     let [username, setUsername] = useState("");
+    let [email, setEmail] = useState("");
    let [password,setPassword] = useState("");
 
    const submitForm = (e)=>{
      e.preventDefault();
 
-     if (email !== "" && password !== ""){
+     if (username !== "" && email !== "" && password !== ""){
        return true
      }
      return false
    }
   return (
  <form onSubmit={submitForm}>
-    <input name="email" autoComplete="username" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+    <input name="username" autoComplete="username" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+    <input name="email" autoComplete="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
     <input name="password" type="password" autoComplete="new-password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
    <button>submit</button>
  </form>
