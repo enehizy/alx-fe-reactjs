58319099077e629565b97287d828da0e44d281fe
enehizy <osamudiameenehizena@gmail.com>
2024-11-26 17:20:00 +0100

    added tailwind

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [["./src/**/*.{js,jsx,ts,tsx}", "./index.html"]],
+  //purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"]
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
