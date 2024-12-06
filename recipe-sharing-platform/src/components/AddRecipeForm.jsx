b7115444f84d676ecbf6fad46d817d318a8f09e4
enehizy <osamudiameenehizena@gmail.com>
2024-12-06 11:00:21 +0100

    added styles

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -10,17 +10,17 @@ const validate=(formData)=>{
 function AddRecipeForm() {
     const [errors,setErrors] =useState();
   return (
-    <form onSubmit={(e)=> handleSubmit(e)}>
-       <label for="title">Recipe Title</label>
+    <form onSubmit={(e)=> handleSubmit(e)} className='shadow-sm md:shadow-lg'>
+       <label for="title" className='text-lg'>Recipe Title</label>
       <input type="text" id="title" name="title" placeholder="Enter recipe title" required/>
 
-    <label for="ingredients">Ingredients</label>
+    <label for="ingredients" className='text-lg'>Ingredients</label>
     <textarea id="ingredients" name="ingredients" rows="5" placeholder="List ingredients, one per line" required></textarea>
 
-    <label for="steps">Preparation Steps</label>
+    <label for="steps" className='text-lg'>Preparation Steps</label>
     <textarea id="steps" name="steps" rows="5" placeholder="Describe the preparation steps, one per line" required></textarea>
 
-    <button type="submit">Submit Recipe</button> 
+    <button type="submit" className='rounded-lg'>Submit Recipe</button> 
     </form>
   )
 }
