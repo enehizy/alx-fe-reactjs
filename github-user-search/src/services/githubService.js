8e2bed8e6d538e1141e24087cff17f7ea7c223c5
enehizy <osamudiameenehizena@gmail.com>
2024-12-14 23:53:00 +0100

    added variables to gitservices

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 export const fetchUserData =async(query)=>{
     try {
       const env = await import.meta.env;
-      
+      // const minRepos =e.target.minimum_repos.value;
+      // const location = e.target.location.value;
     const  result=await axios.get(`https://api.github.com/search/users?q=${query}`,{
       headers : {
         Authorization:`Bearer ${env.VITE_APP_GITHUB_API_KEY}`,
